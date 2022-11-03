1f15ca0b15a2612bbbf266820e651d417b96cf15
Shayann37 <s.gonzalez@students.ephec.be>
2022-11-03 12:28:59 +0100

    new

--- random_passWord.py
+++ random_passWord.py
@@ -6,11 +6,12 @@ def mot_de_passe():
     lower_case = "abcdefghijklmnopqrstuvwxyz"
     upper_case = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
     number = "0123456789"
-    symbol = "@#$€%/?&+-*"
+    symbol = "!«#$%&()*+-./:;><=?@[]^}{|~"
 
-    Use_for = lower_case + upper_case + number + symbol
-    length_to_pass = 12
+    Use_for = lower_case + upper_case + number + symbol 
+    length_to_pass = 15
 
     passWord = "".join(random.sample(Use_for, length_to_pass))
     return passWord
 
+print(mot_de_passe())
